06a543e0023affdd46dc3f4ce742bc431dcf2acc
TheEvilInvader <theevilinvader1@gmail.com>
2025-02-07 01:45:33 +0200

    1st verison

---
+++ Pagejs.js
@@ -0,0 +1,7 @@
+function Button()
+{
+var class1 = document.getElementById("1");
+var class2 = document.getElementById("2");
+class1.setAttribute({class:"Title"},"TileA")
+class2.setAttribute({class:"Button"},"ButtonB")
+}
