0feb7d55fa542b95ea9c91abc04da9b6a8866083
TheEvilInvader <theevilinvader1@gmail.com>
2025-02-12 20:03:42 +0200

    testing fonts

--- Pagejs.js
+++ Pagejs.js
@@ -2,6 +2,28 @@ const resetLevelButton = document.getElementById("resetLevel");
 const resetExpButton = document.getElementById("resetExp");
 const resetToDoListButton = document.getElementById("resetToDoList");
 const resetCalendarEventsButton = document.getElementById("resetCalendarEvents");
+let keySequence = [];
+const targetCombination = ['s', 'o', 'l', 'o','l','e','v','e','l','i','n','g'];
+const targetURL = './devcon.html';
+document.addEventListener('keydown', function(event) 
+{
+    const key = event.key.toLowerCase();
+    if (['s', 'o', 'l', 'o','l','e','v','e','l','i','n','g'].includes(key)) 
+    {
+        keySequence.push(key);
+
+    if (keySequence.join('') === targetCombination.join('')) 
+        {
+        window.location.href = targetURL;
+        }
+
+
+    if (keySequence.length > targetCombination.length) 
+        {
+        keySequence.shift();
+        }
+    }
+});
 
 function resetLevel() 
 {
